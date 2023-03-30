fedc1c1d65a1f37bd2960dd821b1b222656cd628
Nilesh Chate <nilesh.chate@pubmatic.com>
2023-03-30 15:10:47 +0530

    add processed auction hook

---
+++ modules/pubmatic/openwrap/processedauctionhook.go
@@ -0,0 +1,36 @@
+package openwrap
+
+import (
+	"context"
+
+	"github.com/prebid/prebid-server/hooks/hookstage"
+	"github.com/prebid/prebid-server/modules/pubmatic/openwrap/models"
+)
+
+func (m OpenWrap) HandleProcessedAuctionHook(
+	ctx context.Context,
+	moduleCtx hookstage.ModuleInvocationContext,
+	payload hookstage.ProcessedAuctionRequestPayload,
+) (hookstage.HookResult[hookstage.ProcessedAuctionRequestPayload], error) {
+	result := hookstage.HookResult[hookstage.ProcessedAuctionRequestPayload]{}
+	result.ChangeSet = hookstage.ChangeSet[hookstage.ProcessedAuctionRequestPayload]{}
+
+	if len(moduleCtx.ModuleContext) == 0 {
+		return result, nil
+	}
+	rctx, ok := moduleCtx.ModuleContext["rctx"].(models.RequestCtx)
+	if !ok {
+		return result, nil
+	}
+
+	ip := rctx.IP
+
+	result.ChangeSet.AddMutation(func(parp hookstage.ProcessedAuctionRequestPayload) (hookstage.ProcessedAuctionRequestPayload, error) {
+		if parp.BidRequest.Device != nil && (parp.BidRequest.Device.IP == "" && parp.BidRequest.Device.IPv6 == "") {
+			parp.BidRequest.Device.IP = ip
+		}
+		return parp, nil
+	}, hookstage.MutationUpdate, "update-device-ip")
+
+	return result, nil
+}
