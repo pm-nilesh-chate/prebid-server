77deb0827532a6727859b6588fc2bfcdb3dda959
Nilesh Chate <nilesh.chate@pubmatic.com>
2023-04-10 13:23:47 +0530

    fix test = 1

--- modules/pubmatic/openwrap/profiledata.go
+++ modules/pubmatic/openwrap/profiledata.go
@@ -1,11 +1,22 @@
 package openwrap
 
-import "github.com/prebid/prebid-server/modules/pubmatic/openwrap/models"
+import (
+	"github.com/prebid/openrtb/v17/openrtb2"
+	"github.com/prebid/prebid-server/modules/pubmatic/openwrap/models"
+)
 
-func (m OpenWrap) getProfileData(rCtx models.RequestCtx) (map[int]map[string]string, error) {
+func (m OpenWrap) getProfileData(rCtx models.RequestCtx, bidRequest openrtb2.BidRequest) (map[int]map[string]string, error) {
 	if rCtx.IsTestRequest {
+		//get platform from request, since test mode can be enabled for display and app platform only
+		var platform string
+		if bidRequest.Site != nil {
+			platform = models.PLATFORM_DISPLAY
+		} else if bidRequest.App != nil {
+			platform = models.PLATFORM_APP
+		}
+
 		// NYC: can this be clubbed with profileid=0
-		return getTestModePartnerConfigMap(rCtx.PubID, rCtx.ProfileID, rCtx.DisplayID, rCtx.Platform), nil
+		return getTestModePartnerConfigMap(rCtx.PubID, rCtx.ProfileID, rCtx.DisplayID, platform), nil
 	} else if rCtx.ProfileID == 0 {
 		return getDefaultPartnerConfigMap(rCtx.PubID, rCtx.ProfileID, rCtx.DisplayID), nil
 	}
