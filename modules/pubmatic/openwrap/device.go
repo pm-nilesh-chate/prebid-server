550ebdf8f75eb45e6acd7108bdc0b72766bd2f44
Nilesh Chate <nilesh.chate@pubmatic.com>
2023-04-10 12:14:50 +0530

    fix device ext ifa missing cases

--- modules/pubmatic/openwrap/device.go
+++ modules/pubmatic/openwrap/device.go
@@ -6,34 +6,42 @@ import (
 
 	"github.com/prebid/openrtb/v17/openrtb2"
 	"github.com/prebid/prebid-server/modules/pubmatic/openwrap/models"
+	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
 func validateDevice(device *openrtb2.Device) {
 	//unmarshal device ext
 	var deviceExt models.ExtDevice
 	err := json.Unmarshal(device.Ext, &deviceExt)
-	if err != nil || deviceExt.ExtDevice == nil {
+	if err != nil {
 		return
 	}
 
-	deviceExt.IFAType = strings.TrimSpace(deviceExt.IFAType)
-	deviceExt.SessionID = strings.TrimSpace(deviceExt.SessionID)
+	if deviceExt.ExtDevice != nil {
+		deviceExt.IFAType = strings.TrimSpace(deviceExt.IFAType)
+		deviceExt.SessionID = strings.TrimSpace(deviceExt.SessionID)
 
-	//refactor below condition
-	if deviceExt.IFAType != "" {
-		if device.IFA != "" {
-			if _, ok := models.DeviceIFATypeID[deviceExt.IFAType]; !ok {
+		//refactor below condition
+		if deviceExt.IFAType != "" {
+			if device.IFA != "" {
+				if _, ok := models.DeviceIFATypeID[strings.ToLower(deviceExt.IFAType)]; !ok {
+					deviceExt.IFAType = ""
+				}
+			} else if deviceExt.SessionID != "" {
+				device.IFA = deviceExt.SessionID
+				deviceExt.IFAType = models.DeviceIFATypeSESSIONID
+			} else {
 				deviceExt.IFAType = ""
 			}
 		} else if deviceExt.SessionID != "" {
 			device.IFA = deviceExt.SessionID
 			deviceExt.IFAType = models.DeviceIFATypeSESSIONID
-		} else {
-			deviceExt.IFAType = ""
 		}
 	} else if deviceExt.SessionID != "" {
+		deviceExt.ExtDevice = &openrtb_ext.ExtDevice{
+			IFAType: models.DeviceIFATypeSESSIONID,
+		}
 		device.IFA = deviceExt.SessionID
-		deviceExt.IFAType = models.DeviceIFATypeSESSIONID
 	}
 
 	device.Ext, _ = json.Marshal(deviceExt)
