5908b97c81aef9c50e1caf8c7f64a80dfd7e19b0
Nilesh Chate <nilesh.chate@pubmatic.com>
2023-06-06 10:49:20 +0530

    fix test=2

--- modules/pubmatic/openwrap/profiledata.go
+++ modules/pubmatic/openwrap/profiledata.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (m OpenWrap) getProfileData(rCtx models.RequestCtx, bidRequest openrtb2.BidRequest) (map[int]map[string]string, error) {
-	if rCtx.IsTestRequest > 0 {
+	if rCtx.IsTestRequest == 2 { // skip db data for test=2
 		//get platform from request, since test mode can be enabled for display and app platform only
 		var platform string // TODO: should we've some default platform value
 		if bidRequest.App != nil {
